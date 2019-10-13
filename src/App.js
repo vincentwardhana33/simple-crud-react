7aefa4f2ebd56ac112b5aa2a25f832bdbb03b104
Vincent Wardhana <vincentwardhana33@gmail.com>
2019-10-13 13:12:14 +0700

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Sidebar from '../src/Component/Sidebar';
 import Navbar from '../src/Component/Navbar';
